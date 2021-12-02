679b9396f6f2785c0fa982db575722f35c6194e9
duartedark <teteu.duarte8@gmail.com>
2021-12-02 13:59:05 -0300

    Pre_autenticacao

--- paginas/views.py
+++ paginas/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from .models import Task
 from .forms import TaskForm
 from django.contrib import messages
@@ -8,7 +9,14 @@ from django.contrib import messages
 # Create your views here.
 
 def taskList(request):
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks_list = Task.objects.all().order_by('-created_at')
+
+    paginator = Paginator(tasks_list, 5)
+
+    page = request.GET.get('page')
+
+    tasks = paginator.get_page(page)
+
     return render(request, 'paginas/list.html', {'tasks': tasks})
 
 def taskView(request, id):
